4da31596fae4cd90f7eb7a66f79d18a7e29f2f39
Joseph Freeston <freestoj@lafayette.edu>
2025-10-29 15:11:09 -0400

    Upated cmakelists

--- main.c
+++ main.c
@@ -2,13 +2,18 @@
 #include <stdbool.h>
 #include <pico/stdlib.h>
 #include <hardware/pio.h>
-#include <hello.pio.h>
-#define LED_BUILTIN 25;
+#include <blinky.pio.h>
+
+#define LED_BUILTIN 25
+
 int main() {
   stdio_init_all();
   PIO pio = pio0;
   uint state_machine_id = 0;
-  uint offset = pio_add_program(pio, &blinky);  blinky_init(pio, state_machine_id, offset, LED_BUILTIN, 1);
+  uint offset = pio_add_program(pio, &blinky_program);
+  printf("Initializing PIO Blinky\n\r");
+  blinky_init(pio, state_machine_id, offset, LED_BUILTIN);
+
   while(1) {
     //do nothing
   }
