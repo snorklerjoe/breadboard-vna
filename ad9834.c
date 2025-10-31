d225e808199fba097982eb2c12a9c421a4624194
Joseph Freeston <freestoj@lafayette.edu>
2025-10-31 14:46:35 -0400

    created template for ad9834 modules

---
+++ ad9834.c
@@ -0,0 +1,9 @@
+#include "ad9834.h"
+
+void ad98434_init() {
+    // TODO: Init the ad9834
+}
+
+void ad9834_setfreq(unsigned int freq) {
+    // TODO: Set ad9834 freq to given value in kHz
+}
---
+++ ad9834.h
@@ -0,0 +1,11 @@
+/* Simple library for communicating with the ad9834 board
+ */
+
+#ifndef AD9834_H
+#define AD9834_H
+
+void ad98434_init();
+
+void ad9834_setfreq(unsigned int freq);
+
+#endif
