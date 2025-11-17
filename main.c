cc6806857cbc0aa8b0ebaf99d470b5b28b58dc26
MishaGit-boop <petrovm@lafayette.edu>
2025-11-17 14:58:32 -0500

    Moved unused stuff from previous lcd library to another folder

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 #include "pio.h"
 #include "ad9834.h"
 #include <hardware/clocks.h>
-#include "TFTMaster.h"
-#include "ts_lcd.h"
 
 #define LED_BUILTIN 25
 
@@ -22,24 +20,6 @@ static unsigned long int num_entry() {
   return x;
 }
 
-void drawCrossHair(uint16_t xcoor, uint16_t ycoor) {
-    // TODO: Draw a circle at the touch point (10 pixel diameter)
-    // HINT: tft_drawCircle(xcoor, ycoor, radius, color) - radius = 5 for 10px diameter
-    tft_drawCircle(xcoor, ycoor, 5, ILI9340_RED);
-    
-    // TODO: Draw horizontal line through center (10 pixels long)
-    // HINT: tft_drawLine(xcoor-5, ycoor, xcoor+5, ycoor, color)
-    tft_drawLine(xcoor-5, ycoor, xcoor+5, ycoor, ILI9340_RED);
-    
-    // TODO: Draw vertical line through center (10 pixels long)  
-    // HINT: tft_drawLine(xcoor, ycoor-5, xcoor, ycoor+5, color)
-    tft_drawLine(xcoor, ycoor-5, xcoor, ycoor+5, ILI9340_RED);
-    
-    // TODO: Draw a center pixel for better visibility
-    // HINT: tft_drawPixel(xcoor, ycoor, color)
-    tft_drawPixel(xcoor, ycoor, ILI9340_WHITE);
-}
-
 int main() {
   stdio_init_all();
   ts_lcd_init();
@@ -52,16 +32,12 @@ int main() {
   uint16_t touch_x = 100;
   uint16_t touch_y = 100;
 
-  /*
+  
   while(1) {
     unsigned long int freq = num_entry();
     printf("\n\rSetting freq to %ikHz...\n\r", freq);
     // pio_set_sq_freq(AD9834_REF_PIO, sm, (float)freq);
     ad9834_setfreq(freq*1000);
-  };*/
-  
-  while(1) {
-    drawCrossHair(touch_x,touch_y);
-  }
+  };
 
 }
