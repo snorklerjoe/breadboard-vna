374baca319c56d1313b3d60ecb08492bd0319519
Joseph Freeston <freestoj@lafayette.edu>
2025-11-18 20:54:08 -0500

    Added receiver tests to main

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <hardware/pio.h>
 #include "pio.h"
 #include "ad9834.h"
+#include "receiver.h"
 #include <hardware/clocks.h>
 
 #define LED_BUILTIN 25
@@ -20,11 +21,7 @@ static unsigned long int num_entry() {
   return x;
 }
 
-int main() {
-  stdio_init_all();
-
-  sleep_ms(1000);
-
+static void test_stimulus() {
   printf("Initializing AD9834...\n\r");
   ad9834_init();
 
@@ -33,6 +30,34 @@ int main() {
     printf("\n\rSetting freq to %ikHz...\n\r", freq);
     // pio_set_sq_freq(AD9834_REF_PIO, sm, (float)freq);
     ad9834_setfreq(freq*1000);
-  };
+  }
+}
 
+static void test_rx() {
+  printf("Initializing Receiver and AD9834...\n\r");
+  ad9834_init();
+  rx_init();
+  rx_set_incident();
+
+  printf("Please type a frequency.");
+
+  while(1) {
+    unsigned long int freq = num_entry();
+    printf("\n\rSetting freq to %ikHz and LO to %ikHz...\n\r", freq, freq - 1);
+    ad9834_setfreq(freq*1000);
+    rx_setfreq(freq-1);
+  }
+}
+
+int main() {
+  stdio_init_all();
+  sleep_ms(1000);
+  // test_stimulus();
+  test_rx();
+  // test_piofreq();
+
+  // pio_init_losq(pio0, 1, 0, 1);
+  // pio_set_losq_freq(pio0, 1, 1000);
+
+  while(1);
 }
