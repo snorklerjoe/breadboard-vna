61411556b19522b8ae3d00566aa7961d5c720900
MishaGit-boop <petrovm@lafayette.edu>
2025-12-03 18:29:34 -0500

    Xcoord issue

--- main.c
+++ main.c
@@ -322,7 +322,7 @@ int main() {
                     //x is y
                     yLossCoords[i] = graph_return_loss_dB[i];
                     yPhaseCoords[i] = graph_phase_deg[i];
-                    xCoords[i] = 10*log10(graph_frequencies[i]);
+                    xCoords[i] = PPD*log10(graph_frequencies[i]);
                 }
 
                 // Release lock
